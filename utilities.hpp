b3c7e23f0fc88a7a4f5f3e3fdf055ed69a0eaa4c
lschrafs <lschrafs@student.42wolfsburg.de>
2023-01-11 13:48:48 +0100

    11.01.23: fixed ft_make_pair compiler error

--- utilities.hpp
+++ utilities.hpp
@@ -351,7 +351,7 @@ bool operator>=(const ft::pair<T1, T2>& lhs, const ft::pair<T1, T2>& rhs) {
 
 template <class T1, class T2>
 ft::pair<T1, T2> make_pair(T1 t, T2 u) {
-  return ft::pair(t, u);
+  return ft::pair<T1, T2>(t, u);
 }
 
 }  // namespace ft
