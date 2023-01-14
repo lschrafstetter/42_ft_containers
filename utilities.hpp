a8091765ec33fc6152f044906404f4f0083f45b6
lschrafs <lschrafs@student.42wolfsburg.de>
2023-01-14 17:24:37 +0100

    14.01.23: refactores is_equal

--- utilities.hpp
+++ utilities.hpp
@@ -210,8 +210,8 @@ struct is_integral<unsigned long> : public true_type {};
 // https://en.cppreference.com/w/cpp/algorithm/equal
 template <class InputIt1, class InputIt2>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
-  for (; first1 != last1; ++first1, ++first2) {
-    if (!(*first1 == *first2)) {
+  while (first1 != last1) {
+    if (!(*(first1++) == *(first2++))) {
       return false;
     }
   }
@@ -223,8 +223,8 @@ bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
 template <class InputIt1, class InputIt2, class BinaryPredicate>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2,
            BinaryPredicate p) {
-  for (; first1 != last1; ++first1, ++first2) {
-    if (!p(*first1, *first2)) {
+  while (first1 != last1) {
+    if (!p(*(first1++), *(first2++))) {
       return false;
     }
   }
