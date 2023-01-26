4e730c304d1ad7b35e40391c358adc0a39446c14
lschrafs <lschrafs@student.42wolfsburg.de>
2023-01-26 14:52:47 +0100

    26.01.23: added all function declarations and implemented non-member functions

--- map.hpp
+++ map.hpp
@@ -9,7 +9,7 @@ namespace ft {
 template <class Key, class T, class Compare = std::less<Key>,
           class Allocator = std::allocator<std::pair<const Key, T>>>
 class map {
- public: 
+ public:
   //**************************************************
   // Typedefs
   //**************************************************
@@ -30,10 +30,172 @@ class map {
   // std::reverse_iterator<iterator> reverse_iterator;
   // std::reverse_iterator<const_iterator> const_reverse_iterator;
 
- private:
-  
+  //**************************************************
+  // Constructors
+  //**************************************************
 
+  map() {}
+  explicit map(const Compare& comp, const Allocator& alloc = Allocator()) {}
+  template <class InputIt>
+  map(InputIt first, InputIt last, const Compare& comp = Compare(),
+      const Allocator& alloc = Allocator()) {}
+  map(const map& other) {}
+  ~map() {}
+
+  //**************************************************
+  // Member classes
+  //**************************************************
+
+  class value_compare
+      : public std::binary_function<value_type, value_type, bool> {
+   public:
+    typedef bool result_type;
+    typedef value_type first_argument_type;
+    typedef value_type second_argument_type;
+
+   protected:
+    key_compare comp;
+    value_compare() {}
+    value_compare(Compare c) {}
+    bool operator()(const value_type& lhs, const value_type& rhs) const {
+      return comp(lhs, rhs);
+    }
+  };
+
+  //**************************************************
+  // Operator overloads
+  //**************************************************
+
+  map& operator=(const map& other) {}
+
+  //**************************************************
+  // Member functions
+  //**************************************************
+
+  allocator_type get_allocator() const {}
+
+  //**************************************************
+  // Element access
+  //**************************************************
+
+  reference at(const Key& key) {}
+  const_reference at(const Key& key) const {}
+  reference operator[](const Key& key) {}
+
+  //**************************************************
+  // Iterators
+  //**************************************************
+
+  // iterator begin() {}
+  // const_iterator begin() const {}
+  // iterator end() {}
+  // const_iterator end() const {}
+  // reverse_iterator rbegin() {}
+  // const_reverse_iterator rbegin() const {}
+  // reverse_iterator rend() {}
+  // const_reverse_iterator rend() const {}
+
+  //**************************************************
+  // Capacity
+  //**************************************************
+
+  bool empty() const {}
+  size_type size() const {}
+  size_type max_size() const {
+    return std::numeric_limits<difference_type>::max();
+  }
+
+  //**************************************************
+  // Modifiers
+  //**************************************************
+
+  void clear() {}
+  /* std::pair<iterator, bool> insert(const value_type& value) {}
+  iterator insert(iterator pos, const value_type& value) {}
+  template <class InputIt>
+  void insert(InputIt first, InputIt last) {}
+  void erase( iterator pos ) {}
+  void erase( iterator first, iterator last ) {} */
+  size_type erase(const Key& key) {}
+  void swap(map& other) {}
+
+  //**************************************************
+  // Lookup
+  //**************************************************
+
+  size_type count(const Key& key) const {}
+  /* iterator find(const Key& key) {}
+  const_iterator find(const Key& key) const {}
+  std::pair<iterator, iterator> equal_range(const Key& key) {}
+  std::pair<const_iterator, const_iterator> equal_range(const Key& key) const {}
+  iterator lower_bound(const Key& key) {}
+  const_iterator lower_bound(constKey& key) const {}
+  iterator upper_bound(const Key& key) {}
+  const_iterato upper_bound(const Key& key) const {} */
+
+  //**************************************************
+  // Observers
+  //**************************************************
+
+  key_compare key_comp() const {}
+  ft::map<Key, T, Compare, Allocator>::value_comp value_comp() const {}
+
+ private:
 };
+
+//**************************************************
+// Non-member functions
+//**************************************************
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator==(const ft::map<Key, T, Compare, Alloc>& lhs,
+                const ft::map<Key, T, Compare, Alloc>& rhs) {
+  if (lhs.size() != rhs.size()) return false;
+  return ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator!=(const ft::map<Key, T, Compare, Alloc>& lhs,
+                const ft::map<Key, T, Compare, Alloc>& rhs) {
+  return !(lhs == rhs);
+}
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator<(const ft::map<Key, T, Compare, Alloc>& lhs,
+               const ft::map<Key, T, Compare, Alloc>& rhs) {
+  return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(),
+                                     rhs.end());
+}
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator<=(const ft::map<Key, T, Compare, Alloc>& lhs,
+                const ft::map<Key, T, Compare, Alloc>& rhs) {
+  return !(lhs < rhs || lhs == rhs);
+}
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator>(const ft::map<Key, T, Compare, Alloc>& lhs,
+               const ft::map<Key, T, Compare, Alloc>& rhs) {
+  return !(lhs > rhs);
+}
+
+template <class Key, class T, class Compare, class Alloc>
+bool operator>=(const ft::map<Key, T, Compare, Alloc>& lhs,
+                const ft::map<Key, T, Compare, Alloc>& rhs) {
+  return !(lhs < rhs);
+}
+
 }  // namespace ft
+
+namespace std {
+
+// Specialization of the std::swap function
+template <class Key, class T, class Compare, class Alloc>
+void swap(std::map<Key, T, Compare, Alloc>& lhs,
+          std::map<Key, T, Compare, Alloc>& rhs) {
+  lhs.swap(rhs);
+}
+
+}  // namespace std
 
 #endif  // MAP_H
